26e2e79c56a7c5dadf814ef8abb6c89ecb13210b
timmwh <timmeiwirth@gmail.com>
2025-11-12 11:25:35 +0100

    Initial Commit

---
+++ BeFree/BeFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFreeApp.swift
+//  BeFree
+//
+//  Created by Tim Meiwirth on 12.11.25.
+//
+
+import SwiftUI
+
+@main
+struct BeFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeFree/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeFree
+//
+//  Created by Tim Meiwirth on 12.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
